7e28d120f324578467e72fa84c09e1e3849157c7
BrandonScheitlin <Brandon_scheitlin@baylor.edu>
2014-12-04 22:12:31 -0600

    Update point.h

--- point.h
+++ point.h
@@ -1,3 +1,13 @@
+/*
+Authors: Chuks Egbuchunam, Zehao Huang, Jiaqi Ma
+         Brandon Scheitlin, Trent Matthews
+Assignment Title: Breakout
+Assignment Details: Make a game that breaks blocks
+                    with a ball and paddle.
+Due Date: After Thanksgiving
+Date Created: 11/11/2014
+Date Last Modified: 11/12/2014
+*/
 #ifndef POINT_H_INCLUDED
 #define POINT_H_INCLUDED
 
@@ -9,11 +19,39 @@ class Point
 {
 
     public:
-        Point (double xVal = 3, double yVal = 5);
+        // description: set default value of point x and y
+        // return:      no return type
+        // pre:         no
+        // post:        object exists
+        Point (double xVal = 120, double yVal = 95);
+        // description: get value of point x
+        // return:      double
+        // pre:         object exists
+        // post:        return the value of point x
         double getX () const;
+
+        // description: get value of point y
+        // return:      double
+        // pre:         object exists
+        // post:        return the value of point y
         double getY () const;
+
+        // description: set value of point x
+        // return:      no return type
+        // pre:         no
+        // post:        object exists
         void setX (double);
+
+        // description: set value of point y
+        // return:      no return type
+        // pre:         no
+        // post:        object exists
         void setY (double);
+
+        // description: calculate the distance between point x and y
+        // return:      double
+        // pre:         object exists
+        // post:        return the value of distance between point x and y
         double distance (Point p);
 
     private:
@@ -23,4 +61,3 @@ class Point
 
 
 #endif // POINT_H_INCLUDED
-
