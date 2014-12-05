242f83e48730e28687bd8fa557c59129cd36b093
BrandonScheitlin <Brandon_scheitlin@baylor.edu>
2014-12-04 22:16:37 -0600

    Update game.h

--- game.h
+++ game.h
@@ -1,5 +1,13 @@
-
-
+/*
+Authors: Chuks Egbuchunam, Zehao Huang, Jiaqi Ma
+         Brandon Scheitlin, Trent Matthews
+Assignment Title: Breakout
+Assignment Details: Make a game that breaks blocks
+                    with a ball and paddle.
+Due Date: After Thanksgiving
+Date Created: 11/11/2014
+Date Last Modified: 11/12/2014
+*/
 #ifndef GAME_H_INCLUDED
 #define GAME_H_INCLUDED
 
@@ -10,19 +18,31 @@
 class Game
 {
     public:
+    //Description:  set default value of game
+    //Return Type:  none
+    //Pre:          none
+    //Post:         object exists
     Game();
+
+    //Description: Start and run the game
+    //Return Type:  none
+    //Pre:          none
+    //Post:         object exists
     void play();
 
     private:
     int key;
     Plotter screen;
     paddleType paddle;
-    ball gameBall;
+    ball gameBall, fallingBall[240];
     brick wall[WALL_ROWS][WALL_COLS];
     sideBoundariesReflections Reflections;
     bool gameOver, active;
+    Point point;
 
 
 };
+
+#endif // GAME_H_INCLUDED
 
 #endif // GAME_H_INCLUDED
